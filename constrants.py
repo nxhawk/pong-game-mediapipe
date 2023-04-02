9054cc663c8714febee994f1735fad436430cda5
haonhat0703 <haonhat2729@gmail.com>
2023-04-02 20:09:49 +0700

    add color define

--- constrants.py
+++ constrants.py
@@ -8,10 +8,20 @@ urlGameOver = './img/gameOver.png'
 urlBall = './img/Ball.png'
 urlBat1 = './img/bat1.png'
 urlBat2 = './img/bat2.png'
+urlControl = './img/control.png'
 
 # define value start game
-definePos = [100, 100]
 speedX = 15
 speedY = 15
 gameOver = False
 score = [0, 0]
+player = 2
+
+# Color define
+WHITE = (255, 255, 255)
+RED = (0, 0, 255)
+GREEN = (51, 102, 0)
+BLUE = (255, 0, 0)
+GRAY = (25, 0, 0)
+BLUR_RED = (51, 51, 255)
+BLUR_GREEN = (51, 102, 0)
