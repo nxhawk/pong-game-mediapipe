4a7bd81840ae6e546d59a6ca2633230743c296da
haonhat0703 <haonhat2729@gmail.com>
2023-04-02 16:02:33 +0700

    add define value start game

--- constrants.py
+++ constrants.py
@@ -1,7 +1,17 @@
+# size main screen game
 Screen_Width = 1280
 Screen_Height = 720
+
+# url to image game
 urlBackGround = './img/Background.png'
 urlGameOver = './img/gameOver.png'
 urlBall = './img/Ball.png'
 urlBat1 = './img/bat1.png'
 urlBat2 = './img/bat2.png'
+
+# define value start game
+definePos = [100, 100]
+speedX = 15
+speedY = 15
+gameOver = False
+score = [0, 0]
