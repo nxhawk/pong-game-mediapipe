39288e5c47f193c428d07a0c2f919c4df0bb8307
haonhat0703 <haonhat2729@gmail.com>
2023-04-02 15:58:58 +0700

    define value for game

---
+++ constrants.py
@@ -0,0 +1,7 @@
+Screen_Width = 1280
+Screen_Height = 720
+urlBackGround = './img/Background.png'
+urlGameOver = './img/gameOver.png'
+urlBall = './img/Ball.png'
+urlBat1 = './img/bat1.png'
+urlBat2 = './img/bat2.png'
